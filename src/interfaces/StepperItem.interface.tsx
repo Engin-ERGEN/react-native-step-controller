2d1f4b025dc030bb91b99a97c6d188071dc83362
Engin ERGEN <engin.ergen@hotmail.com>
2025-02-11 22:52:14 +0300

    feat: title required is removed

--- src/interfaces/StepperItem.interface.tsx
+++ src/interfaces/StepperItem.interface.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
 
 export interface StepperItemProps {
-  title: string;
+  title?: string;
   children: React.ReactNode;
   contentContainerStyle?: StyleProp<ViewStyle> | null;
 }
